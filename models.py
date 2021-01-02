bf68daa985ea3dee058a4394af1c0418f45b80c9
Darrell L. Mayberry <dmayberr@gmail.com>
2021-01-01 19:23:20 -0500

    Created Model.py with model for Cupcake.

--- models.py
+++ models.py
@@ -1,1 +1,31 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    
+class Cupcake(db.Model):
+    """Cupcake model"""
+    
+    def __repr__(self):
+        u = self
+        return f"<id={u.id}"
+    
+    id = db.Column(db.Integer, 
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.String(25),
+                       nullable=False)
+    size = db.Column(db.String(10),
+                     nullable=False)
+    rating = db.Column(db.Float(precision=3),
+                       nullable=False)
+    image = db.Column(db.String(200),
+                      nullable=False,
+                      default='https://tinyurl.com/demo-cupcake')
+    
+    
